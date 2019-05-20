f146bfcb2760689f1c31fc03b296d8bfe39df2f8
sreekanth.sreekumar <sreekanth.s@dspg.com>
2019-05-20 13:45:28 +0000

    Completed adding the approximate skeleton for PLs (Weeks 2-9).

---
+++ sms-tools/lectures/PLs/W5/P2-2.py
@@ -0,0 +1,35 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.pyplot as pltx
+import matplotlib.pyplot as pltmX
+import matplotlib.pyplot as pltpX
+import matplotlib.pyplot as pltB
+import matplotlib.pyplot as pltmBX
+import matplotlib.pyplot as pltpBX
+from scipy.signal import get_window
+from scipy.fftpack import fft
+import sys, os, math
+
+"""
+P2-2: Sinusoidal analysis using genspeclines
+
+This program implements additive synthesis using sinusoidal model by genspeclines.
+
+"""
+
+
+
+cwd = os.getcwd()
+dtree = cwd.split('/')
+ltree = len(dtree)
+lroot = ltree - 3
+stpath =""
+stpath = '/'.join(dtree[:lroot])
+modpath = stpath + '/software/models/'
+sys.path.append(modpath)
+
+
+import utilFunctions as UF
+
+bins = np.array([-4, -3, -2, -1, 0, 1, 2, 3])
+x = UF.genBhLobe(bins)
---
+++ sms-tools/lectures/PLs/W5/P3-1.py
@@ -0,0 +1,35 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.pyplot as pltx
+import matplotlib.pyplot as pltmX
+import matplotlib.pyplot as pltpX
+import matplotlib.pyplot as pltB
+import matplotlib.pyplot as pltmBX
+import matplotlib.pyplot as pltpBX
+from scipy.signal import get_window
+from scipy.fftpack import fft
+import sys, os, math
+
+"""
+P3-1: Sinusoidal tracking using sinusoidal model
+
+This program implements additive synthesis using sinusoidal model.
+
+"""
+
+
+
+cwd = os.getcwd()
+dtree = cwd.split('/')
+ltree = len(dtree)
+lroot = ltree - 3
+stpath =""
+stpath = '/'.join(dtree[:lroot])
+modpath = stpath + '/software/models/'
+sys.path.append(modpath)
+
+
+import utilFunctions as UF
+
+bins = np.array([-4, -3, -2, -1, 0, 1, 2, 3])
+x = UF.genBhLobe(bins)
