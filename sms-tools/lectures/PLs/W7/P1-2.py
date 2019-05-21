d3f898ffbedc45f3f3947db1f820efd69173e61c
sreekanth.sreekumar <sreekanth.s@dspg.com>
2019-05-21 17:13:08 +0000

    Completed updating Week 7 code.

--- sms-tools/lectures/PLs/W7/P1-2.py
+++ sms-tools/lectures/PLs/W7/P1-2.py
@@ -6,8 +6,8 @@ import matplotlib.pyplot as pltpX
 import matplotlib.pyplot as pltB
 import matplotlib.pyplot as pltmBX
 import matplotlib.pyplot as pltpBX
-from scipy.signal import get_window
-from scipy.fftpack import fft
+from scipy.signal import get_window, resample
+from scipy.fftpack import fft, ifft
 import sys, os, math
 
 """
@@ -19,92 +19,31 @@ This program implements analysis of a window function using DFT.
 
 
 
-M = 63 # Window length/size.
-# Generates a smoothing window of type 'hanning', a raised cosine function.
-window = get_window('hanning', M)
-# plt.plot(window)
-# plt.show()
+cwd = os.getcwd()
+dtree = cwd.split('/')
+ltree = len(dtree)
+lroot = ltree - 3
+stpath = ""
+stpath = '/'.join(dtree[:lroot])
+modpath = stpath + '/software/models/'
+sys.path.append(modpath)
 
-# The following variables store the middle of the window information, irrespective of whether
-# the window size is an even or odd number.
-hM1 = int(math.floor((M + 1)) / 2)
-hM2 = int(math.floor(M / 2))
 
-# The following section prepares the window for computing the FFT. In order to do that, we
-# need to do zero-phase windowing - place the window at the zeroth location (shift and plot in
-# a way that it's centred around zero). So, we create a buffer 'fftbuffer' of the size of the
-# FFT and we place the window around the zeroth sample by splitting the samples into two halves
-# and shifting the second half to the left side (the beginning of the new buffer) and the first
-# half to the right side (the end of the new buffer). The spectrum is calculated on the data of
-# this resulting buffer called 'fftbuffer'.
-N = 512
-hN = N / 2 # 'hN' version 1.0
-# hN = (N / 2) + 1 # 'hN' version 2.0
-fftbuffer = np.zeros(N)
-fftbuffer[:hM1] = window[hM2:]
-fftbuffer[N - hM2:] = window[:hM2]
-# plt.plot(fftbuffer)
-# plt.show()
+import utilFunctions as UF
 
-# To plot the magnitude spectrum in 'dB', we need to make sure that there are no zeroes in the
-# absolute value of the result of FFT operation on 'fftbuffer'. This is because for calculating
-# the magnitude in 'dB', we take the logarithm and 'log(0)' is "UNDEFINED". This is done by
-# checking if the absolute value is less than epsilon value, the minimum representable value in
-# python, which has to be zero (magnitude is always positive) or thereabouts and if any such
-# value is found, proceed by replacing it with epsilon value. This way undefined values can be
-# avoided. It is to be noted that the first part of magnitude spectrum will have +ve frequency
-# values and the second part will have negative frequency values.
-X = fft(fftbuffer)
-absX = abs(X)
-# plt.plot(absX)
-# plt.show()
-absX[absX < np.finfo(float).eps] = np.finfo(float).eps
-# plt.plot(absX)
-# plt.show()
-mX = 20 * np.log10(absX)
-plt.plot(mX)
-plt.show()
-pX = np.angle(X)
-plt.plot(pX)
-plt.show()
+inputFile = stpath + '/sounds/ocean.wav'
+fs, x = UF.wavread(inputFile)
+M = N = 256
+stocf = 0.2
+window_type = 'hamming'
+w = get_window(window_type, M)
+xw = x[10000:10000 + M]
+X = fft(xw)
+mX = 20 * np.log10(abs(X[:N/2]))
+mXenv = resample(np.maximum(-200, mX), mX.size * stocf)
 
-# The following part nullifies/reverses/undo's the zero-phase windowing operation done earlier in
-# 'fftbuffer' in order to make the data easier to visualize. This 'mX1' is a better way display
-# (by moving the main lobe to the center) the magnitude spectrum. In the phase spectrum, the most
-# important observation is the phases in the middle. Though we have (2 * pi) discontinuities, it
-# can be deduced as having a value zero at every point.
-mX1 = np.zeros(N)
-pX1 = np.zeros(N)
-mX1[:hN] = mX[hN:] # For 'hN' version 1.0
-mX1[hN:] = mX[:hN] # For 'hN' version 1.0
-# mX1[:hN] = mX[N - hN:] # For 'hN' version 2.0
-# mX1[N - hN:] = mX[:hN] # For 'hN' version 2.0
-plt.plot(mX1)
-plt.show()
-pX1[:hN] = pX[hN:] # For 'hN' version 1.0
-pX1[hN:] = pX[:hN] # For 'hN' version 1.0
-# pX1[:hN] = pX[N - hN:] # For 'hN' version 2.0
-# pX1[N - hN:] = pX[:hN] # For 'hN' version 2.0
-plt.plot(pX1)
-plt.show()
-
-# The following allow us to visualize the x-axis of the magnitude spectrum better. Basically, we
-# have normalized the horizontal axis (x-axis) by dividing by N and multiplying by M so that we
-# actually see the samples with respect to the window. Also, the magnitude is normalized so that
-# the maximum value is 0 dB.
-plt.plot(np.arange(-hN, hN) / float(N) * M, mX1 - max(mX1))
-plt.axis([-20, 20, -80, 0])
-plt.show()
-
-# plt.plot(x) # Plots the amplitude-sample graph of x[n]
-# plt.plot(tx, x) # Plots the amplitude-time graph of x[n] with time in seconds
-# plt.plot(tx1, x1) # Plots the amplitude-time graph of x1[n] with time in seconds
-# plt.axis([start_time, stop_time, min(x1), max(x1)]) # Makes a tight plot of x1[n]
-# plt.plot(w) # Plots the amplitude-sample graph of smoothing window 'w'
-# plt.plot(mX1) # Plots the magnitude spectrum of x1[n] (in dB), this plots only the positive side
-# plt.show()    # of the spectrum, going from (half of sampling rate) 0 to N/2 (half of FFT size).
-# plt.plot(pX1) # Plots the phase spectrum of x1[n], with phase unwrap applied.
-# plt.axis([-0.2, 0.9, -0.8, 0.8])
-# plt.xlabel("time")
-# plt.ylabel("amplitude")
-# plt.show()
+mY = resample(mXenv, N/2)
+pY = 2 * np.pi * np.random.rand(N/2)
+Y = np.zeros(N, dtype=complex)
+Y[:N/2] = 10 ** (mY/20) * np.exp(-1j * pY[:0:-1])
+y = np.real(ifft(Y))
--- sms-tools/lectures/PLs/W7/P3-1.py
+++ sms-tools/lectures/PLs/W7/P3-1.py
@@ -6,7 +6,7 @@ import matplotlib.pyplot as pltpX
 import matplotlib.pyplot as pltB
 import matplotlib.pyplot as pltmBX
 import matplotlib.pyplot as pltpBX
-from scipy.signal import get_window
+from scipy.signal import get_window, resample
 from scipy.fftpack import fft
 import sys, os, math
 
@@ -19,92 +19,58 @@ This program implements analysis of a window function using DFT.
 
 
 
-M = 63 # Window length/size.
-# Generates a smoothing window of type 'hanning', a raised cosine function.
-window = get_window('hanning', M)
-# plt.plot(window)
-# plt.show()
+cwd = os.getcwd()
+dtree = cwd.split('/')
+ltree = len(dtree)
+lroot = ltree - 3
+stpath = ""
+stpath = '/'.join(dtree[:lroot])
+modpath = stpath + '/software/models/'
+sys.path.append(modpath)
 
-# The following variables store the middle of the window information, irrespective of whether
-# the window size is an even or odd number.
-hM1 = int(math.floor((M + 1)) / 2)
+
+import utilFunctions as UF
+import dftModel as DFT
+import harmonicModel as HM
+
+inputFile = stpath + '/sounds/oboe-A4.wav'
+fs, x = UF.wavread(inputFile)
+pin = 40000
+M = 801
+N = 2048
+t = -80
+minf0 = 300
+maxf0 = 500
+f0et = 5
+nH = 60
+harmDevSlope = 0.001
+stocf = 0.4
+
+window_type = 'blackman'
+w = get_window(window_type, M)
+hM1 = int(math.floor((M + 1) / 2))
 hM2 = int(math.floor(M / 2))
 
-# The following section prepares the window for computing the FFT. In order to do that, we
-# need to do zero-phase windowing - place the window at the zeroth location (shift and plot in
-# a way that it's centred around zero). So, we create a buffer 'fftbuffer' of the size of the
-# FFT and we place the window around the zeroth sample by splitting the samples into two halves
-# and shifting the second half to the left side (the beginning of the new buffer) and the first
-# half to the right side (the end of the new buffer). The spectrum is calculated on the data of
-# this resulting buffer called 'fftbuffer'.
-N = 512
-hN = N / 2 # 'hN' version 1.0
-# hN = (N / 2) + 1 # 'hN' version 2.0
-fftbuffer = np.zeros(N)
-fftbuffer[:hM1] = window[hM2:]
-fftbuffer[N - hM2:] = window[:hM2]
-# plt.plot(fftbuffer)
-# plt.show()
+x1 = x[pin - hM1:pin + hM2]
+mX1, pX1 = DFT.dftAnal(x1, w, N)
+ploc = UF.peakDetection(mX1, t)
+iploc, ipmag, ipphase = UF.peakInterp(mX1, pX1, ploc)
+ipfreq = fs * iploc / N
+f0 = UF.f0Twm(ipfreq, ipmag, f0et, minf0, maxf0, 0)
+hfreq, hmag, hphase = HM.harmonicDetection(ipfreq, ipmag, ipphase, f0, nH, [], fs, harmDevSlope)
 
-# To plot the magnitude spectrum in 'dB', we need to make sure that there are no zeroes in the
-# absolute value of the result of FFT operation on 'fftbuffer'. This is because for calculating
-# the magnitude in 'dB', we take the logarithm and 'log(0)' is "UNDEFINED". This is done by
-# checking if the absolute value is less than epsilon value, the minimum representable value in
-# python, which has to be zero (magnitude is always positive) or thereabouts and if any such
-# value is found, proceed by replacing it with epsilon value. This way undefined values can be
-# avoided. It is to be noted that the first part of magnitude spectrum will have +ve frequency
-# values and the second part will have negative frequency values.
-X = fft(fftbuffer)
-absX = abs(X)
-# plt.plot(absX)
-# plt.show()
-absX[absX < np.finfo(float).eps] = np.finfo(float).eps
-# plt.plot(absX)
-# plt.show()
-mX = 20 * np.log10(absX)
-plt.plot(mX)
-plt.show()
-pX = np.angle(X)
-plt.plot(pX)
-plt.show()
+Ns = 512
+hNs = 256
+Yh = UF.genspecSines(hfreq, hmag, Ns, fs)
 
-# The following part nullifies/reverses/undo's the zero-phase windowing operation done earlier in
-# 'fftbuffer' in order to make the data easier to visualize. This 'mX1' is a better way display
-# (by moving the main lobe to the center) the magnitude spectrum. In the phase spectrum, the most
-# important observation is the phases in the middle. Though we have (2 * pi) discontinuities, it
-# can be deduced as having a value zero at every point.
-mX1 = np.zeros(N)
-pX1 = np.zeros(N)
-mX1[:hN] = mX[hN:] # For 'hN' version 1.0
-mX1[hN:] = mX[:hN] # For 'hN' version 1.0
-# mX1[:hN] = mX[N - hN:] # For 'hN' version 2.0
-# mX1[N - hN:] = mX[:hN] # For 'hN' version 2.0
-plt.plot(mX1)
-plt.show()
-pX1[:hN] = pX[hN:] # For 'hN' version 1.0
-pX1[hN:] = pX[:hN] # For 'hN' version 1.0
-# pX1[:hN] = pX[N - hN:] # For 'hN' version 2.0
-# pX1[N - hN:] = pX[:hN] # For 'hN' version 2.0
-plt.plot(pX1)
-plt.show()
+wr = get_window('blackmanharris', Ns)
+xw2 = x[pin - hNs -1: pin + hNs -1] * wr / sum(wr)
+fftbuffer = np.zeros(Ns)
+fftbuffer[:hNs] = xw2[hNs:]
+fftbuffer[hNs:] = xw2[:hNs]
+X2 = fft(fftbuffer)
+Xr = X2 - Yh
 
-# The following allow us to visualize the x-axis of the magnitude spectrum better. Basically, we
-# have normalized the horizontal axis (x-axis) by dividing by N and multiplying by M so that we
-# actually see the samples with respect to the window. Also, the magnitude is normalized so that
-# the maximum value is 0 dB.
-plt.plot(np.arange(-hN, hN) / float(N) * M, mX1 - max(mX1))
-plt.axis([-20, 20, -80, 0])
-plt.show()
-
-# plt.plot(x) # Plots the amplitude-sample graph of x[n]
-# plt.plot(tx, x) # Plots the amplitude-time graph of x[n] with time in seconds
-# plt.plot(tx1, x1) # Plots the amplitude-time graph of x1[n] with time in seconds
-# plt.axis([start_time, stop_time, min(x1), max(x1)]) # Makes a tight plot of x1[n]
-# plt.plot(w) # Plots the amplitude-sample graph of smoothing window 'w'
-# plt.plot(mX1) # Plots the magnitude spectrum of x1[n] (in dB), this plots only the positive side
-# plt.show()    # of the spectrum, going from (half of sampling rate) 0 to N/2 (half of FFT size).
-# plt.plot(pX1) # Plots the phase spectrum of x1[n], with phase unwrap applied.
-# plt.axis([-0.2, 0.9, -0.8, 0.8])
-# plt.xlabel("time")
-# plt.ylabel("amplitude")
-# plt.show()
+mXr = 20 * np.log10(abs(Xr[:hNs]))
+mXrenv = resample(np.maximum(-200, mXr), mXr.size * stocf)
+stocEnv = resample(mXrenv, hNs)
